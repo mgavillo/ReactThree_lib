81514d20cba826b80d22e9e8374dd6ebcb8b9f62
mgavillo <mgavillo@student.42.fr>
2022-01-05 19:43:39 +0100

    added reflectign ball

--- src/App.js
+++ src/App.js
@@ -22,14 +22,31 @@ const Skybox = () => {
     return(null);
 }
 
+const Sphere = () => {
+    const { scene, gl } = useThree();
+    
+    const cubeRenderTarget = new THREE.WebGLCubeRenderTarget(256, { format: THREE.RGBFormat, generateMipmaps: true, minFilter: THREE.LinearMipmapLinearFilter,});
+    const cubeCamera = new THREE.CubeCamera(1, 1000, cubeRenderTarget);
+    cubeCamera.position.set(0, 100, 0);
+    scene.add(cubeCamera);  // Update the cubeCamera with current renderer and scene.
+    useFrame(() => cubeCamera.update(gl, scene));
+    return(
+        <mesh position={[0, 100, 0]}>
+            <sphereBufferGeometry args={[350, 50, 50]} attach="geometry" />
+            <meshBasicMaterial color={0xfff1ef} envMap={cubeCamera.renderTarget.texture} attach="material" />
+        </mesh>
+    );
+}
+
 export default function App (){
     return(
-        <Canvas shadows camera={{position:[1200, -250, 20000], fov:[55]}} 
+        <Canvas shadows camera={{position:[0, 1, 10], fov:[55]}} 
         style={{ width: window.innerWidth, height: window.innerHeight}}>
             <CameraControls />
-            <Suspense fallback={<Html>"loading...."</Html>}>
+            <Suspense fallback={<Html>"loading...."</Html>}> */
                 <Skybox/>
             </Suspense>
+            <Sphere />
         </Canvas>
     );
 };
