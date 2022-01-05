a7f5f5375676a0d6ab55e95064fd6c53da25a41c
mgavillo <mgavillo@student.42.fr>
2022-01-05 16:44:00 +0100

    matcap finishedddd

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
 import { useLoader } from '@react-three/fiber'
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import * as THREE from 'three'
-import { AmbientLight } from 'three';
 
 softShadows()
 extend({ OrbitControls});
@@ -30,53 +29,48 @@ function getMethods(obj)
 
 function ObjToPrimitive({ url, mat }) {
     const [obj, setObj] = useState();
-    useMemo(() => new OBJLoader().load(url, setObj), [url]);
+    const manager = new THREE.LoadingManager();
+    let geometry = null;
+    useMemo(() => new OBJLoader(manager).load(url, setObj), [url]);
     if (obj) {
       obj.traverse((child) => {
-        if (child.isMesh) {
-            child.castShadow = true;
-            // child.material = mat;
-            // child.isMeshMatcapMaterial = true;
-            if ( child.material.matcap )
-                child.material.matcap.dispose();
-                mat.needsUpdate = true;
-                child.material.needsUpdate = true;
-                child.material = mat;
-                // console.log(child.material.isMeshMatcapMaterial)
-            console.log(getMethods(child.material))
-        }
+          if (child.isMesh)
+            geometry = child.geometry;
       });
-      return <primitive object={obj} />;
+      return geometry;
     }
+    console.log("prout")
     return null;
   }
 
 const Dragon = () => {
-    // const obj = useLoader(OBJLoader, '/stanford-dragon.obj');
-    const [map] = useLoader(THREE.TextureLoader, ["/matcap_dark.png"])
-    var mat = new THREE.MeshMatcapMaterial();
-    mat.map = map;
+    const [map] = useLoader(THREE.TextureLoader, ["/matcap_rainbow.png"])
+    map.encoding = THREE.sRGBEncoding;
+    const mat = new THREE.MeshMatcapMaterial();
+    mat.matcap = map;
+    let geometry= null;
+    geometry = ObjToPrimitive({ url: "/stanford-dragon.obj", mat })
+    if (geometry !== null){
+        return(
+            <mesh geometry={geometry} material={mat} color="#000000"/>
+        );
+    }
     return(
-        <mesh>
-            {ObjToPrimitive({ url: "/stanford-dragon.obj", mat })}
-        </mesh>
+        <Html>Still loading...</Html>
     );
+
 }
 
 export default function App (){
 
-  
     return(
-        <Canvas shadows camera={{position:[0, 1, 10], fov:[65]}} 
+        <Canvas shadows camera={{position:[0, 1, 10], fov:[65]} } colorManagement={false}
         style={{ width: window.innerWidth, height: window.innerHeight}}>
             <color attach="background" args={["blue"]} />
             <CameraControls />
             <Suspense fallback={<Html><h1>Loading profile...</h1></Html>}>
-                {/* <Stage> */}
-                    <Dragon/>
-                {/* </Stage> */}
+                <Dragon/>
             </Suspense>
-            <ambientLight/>
         </Canvas>
     );
 };
